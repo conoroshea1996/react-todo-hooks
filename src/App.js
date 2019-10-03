e28fe9d1f22f7f654b0ace09b753459a16e07da6
conoroshea1996 <conoshea12@gmail.com>
2019-10-03 18:01:23 +0100

    add Images for To Do status

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'reactstrap';
+import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
 
 
 function App() {
   const [todosList, setTodosList] = useState([
-    { id: 1, todo: 'use hooks', done: false },
+    { id: 1, todo: 'use hooks', done: true },
     { id: 2, todo: 'hooks are awesome', done: false },
     { id: 3, todo: 'hooks are fun', done: false }
   ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todosList, text];
+    setTodosList(newTodos);
+  }
+
+  const isDone = (num) => {
+    const newTodos = [...todosList];
+    newTodos[num].done = !newTodos[num].done;
+    setTodosList(newTodos);
+  }
 
   return (
     <div className="App">
@@ -16,10 +29,11 @@ function App() {
         <Row>
           {todosList.map((todo, index) => (
             <Col sm='12' md='6' lg='4'>
-              <Todo key={todo.id} index={index} todo={todo} />
+              <Todo key={todo.id} index={index} todo={todo} isDone={isDone} />
             </Col>
           ))}
         </Row>
+        <TodoForm addTodo={addTodo} />
       </Container>
     </div>
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
-const Todo = ({ todo, index }) => {
+import Cross from '../images/cross.jpeg';
+import Tick from '../images/Tick.png';
+
+const Todo = ({ todo, index, isDone }) => {
+    console.log(todo);
     return (
         <div>
-
+            <Card>
+                <CardImg style={{ height: '200px' }} top width="100%" src={todo.done ? Tick : Cross} alt="Todo-Status" />
+                <CardBody style={{ textAlign: 'center' }}>
+                    <CardTitle > TO-DO</CardTitle>
+                    <CardText>{todo.todo}</CardText>
+                    <Button block onClick={() => isDone(index)}>Done</Button>
+                </CardBody>
+            </Card>
         </div>
     )
 }
