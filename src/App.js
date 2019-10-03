a63264fa31acce3e5ca990a0556fee8b04375940
conoroshea1996 <conoshea12@gmail.com>
2019-10-03 16:46:08 +0100

    create Todo Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'reactstrap';
+
 
 function App() {
   const [todosList, setTodosList] = useState([
     { id: 1, todo: 'use hooks', done: false },
     { id: 2, todo: 'hooks are awesome', done: false },
     { id: 3, todo: 'hooks are fun', done: false }
-  ])
+  ]);
 
   return (
     <div className="App">
-
+      <Container >
+        <Row>
+          {todosList.map((todo, index) => (
+            <Col sm='12' md='6' lg='4'>
+              <Todo key={todo.id} index={index} todo={todo} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = ({ todo, index }) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Todo
